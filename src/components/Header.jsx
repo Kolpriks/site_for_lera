0af9e031895566f442e8c417336c2d6e0408f352
Kolpriks <126422218+Kolpriks@users.noreply.github.com>
2024-01-24 01:58:16 +0300

    Upload (header)

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='header_photo'>
+
+          <div>
+            <a href='/'>ЦЕНЫ</a>
+            <a href='/'>ЧТО-ТО</a>
+          </div>
+
+          <div>LOGO</div>
+          
+          <div>
+            <a href='/'>ПРАВИЛА</a>
+            <a href='/'>КОНТАКТЫ</a>
+          </div>
+
+        </div>
+      </div>
+    
+    </>
+  )
+}
+
