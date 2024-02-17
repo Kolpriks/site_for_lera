f9e76d9880d5a7af27da6808e7e4247af8ccfff3
Kolpriks <vladkulikov348@gmail.com>
2024-02-17 05:45:44 +0300

    Hot Fix #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Footer from "../components/footer/Footer";
-import { Header } from "../components/header/Header";
+
 import Portfolio from "../components/portfolio/Portfolio";
 import Price from "../components/price/Price";
 import Steps from "../components/steps/Steps";
@@ -11,7 +11,6 @@ export default function Home() {
 	return (
 		<div className="All">
 			<Main/>
-			{/* <Header/> */}
 			<WhyAs/>
 			<Portfolio/>
 			<Steps/>
